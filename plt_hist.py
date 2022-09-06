cf9c8a4a54342a86caef9bb454674c03e916087b
Johnnie Walker <jw12866@gmail.com>
2022-09-06 07:53:15 -0400

    fixing logging to stdout and data_out.csv

--- plt_hist.py
+++ plt_hist.py
@@ -11,6 +11,7 @@ actuals_high = {}
 actuals_low = {}
 fcast = []
 fcasts = []
+row_count = 0
 
 # open actuals file and store highs and lows in separate dicts
 # with date as key for each
@@ -24,8 +25,12 @@ else:
 with open(f_act) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
+        row_count += 1
         actuals_high[datetime.datetime.strptime(row[1], "%Y-%m-%d")] = row[2]
         actuals_low[datetime.datetime.strptime(row[1], "%Y-%m-%d")] = row[3]
+
+print('Read ' + str(row_count) + ' rows from actuals.csv')
+row_count = 0
 
 # open fcasts CSV and store rows in list of lists fcasts
         
@@ -43,6 +48,10 @@ with open(f_fct) as csv_file:
         fcast.append(row[4])
         fcasts.append(fcast)
         fcast = []
+        row_count += 1
+
+print('Read ' + str(row_count) + ' rows from fcasts.csv')
+row_count = 0
 
 # initialize the data list of lists for each of high and low
 # format then the first index key is days between actual and origin of forecast date
@@ -60,7 +69,7 @@ if socket.gethostname() == "sparta":
     f_do = '/home/johnnie/wxstat/data_out.csv'
 else:    
     f_do = 'data_out.csv'
-f = open(f_do, 'w', newline='')
+f = open(f_do, 'w')
 writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_ALL)
 
 for row in fcasts:
@@ -72,6 +81,8 @@ for row in fcasts:
             data_out.append('High')
             data_out.append(actuals_high[row[0]])
             writer.writerow(data_out)
+            data_out= []
+            row_count += 1
             data_high[(row[0]-row[1]).days].append(round(float(row[3]) - \
                                                          float(actuals_high[row[0]]), 2))
     elif row[2] == "Low":
@@ -82,11 +93,22 @@ for row in fcasts:
             data_out.append('Low')
             data_out.append(actuals_low[row[0]])
             writer.writerow(data_out)
+            data_out = []
+            row_count += 1
             data_low[(row[0]-row[1]).days].append(round(float(row[3]) -
                                                         float(actuals_low[row[0]]), 2))
 f.close()
-    
-num_bins = 5
+
+print('Wrote ' + str(row_count) + ' rows to data_out.csv')
+print('Data available for plotting :')
+print('HIGH -->')
+for item in data_high:
+    print('- ' + str(item))
+print('LOW -->')
+for item in data_low:
+    print('- ' + str(item))
+
+num_bins = 20
 mint = -15
 maxt = 15
 
@@ -100,8 +122,8 @@ for idx, ax in enumerate(a):
 
 pyplot.xlim([mint,maxt])
 pyplot.tight_layout()
-pyplot.savefig('/home/johnnie/wxstat/hist_high.png')
-# pyplot.savefig('/home/johnnie/Projects/wxstat/hist_high.png')
+# pyplot.savefig('/home/johnnie/wxstat/hist_high.png')
+pyplot.savefig('/home/johnnie/Projects/wxstat/hist_high.png')
 print('Saved High Histograms')
 
 f, a = pyplot.subplots(4, 1, sharex = True)
@@ -114,6 +136,6 @@ for idx, ax in enumerate(a):
 
 pyplot.xlim([mint,maxt])
 pyplot.tight_layout()
-pyplot.savefig('/home/johnnie/wxstat/hist_low.png')
-# pyplot.savefig('/home/johnnie/Projects/wxstat/hist_low.png')
+# pyplot.savefig('/home/johnnie/wxstat/hist_low.png')
+pyplot.savefig('/home/johnnie/Projects/wxstat/hist_low.png')
 print('Saved Low Histograms')
